e04ef81ad23f3bc8b527f70298a30f8062ba3713
AleksDark <aleks.dark@inbox.ru>
2018-05-02 16:50:36 +0300

    Update

---
+++ copyCommand.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp /home/sofiev/University/SyventavatLabrat/DWI/tex/DWI_labra.pdf /home/sofiev/University/SyventavatLabrat/DWI/git/SyvLabDWI
+
+git add /home/sofiev/University/SyventavatLabrat/DWI/git/SyvLabDWI/
+
+git commit -m "Update"
+
+git push
